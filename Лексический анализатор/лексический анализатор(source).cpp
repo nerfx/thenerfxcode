76bed0ed8b79024a4b4d156b9d584c53a8a99fb3
nerfx <79503280+nerfx@users.noreply.github.com>
2021-11-12 06:59:59 +0300

    фундаментальные алгоритмы

---
+++ Лексический анализатор/лексический анализатор(source).cpp
@@ -0,0 +1,236 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+using namespace std;
+
+template <typename T>  
+void output(vector<T> A, int N) {
+	for (int i = 0; i < N; ++i) {
+		cout << A[i] << endl;
+	}
+}
+
+vector<string> delete_tab(vector<string> B, int size) { 
+	for (int i = 0; i < size; ++i) {
+		if (B[i][0] == '\t') {
+			B[i].erase(0, 1);
+		}
+	}
+	vector<string> add_arr;
+	for (int i = 0; i < size; ++i) {
+		add_arr.push_back(B[i]);
+	}
+	return add_arr;
+}
+
+vector<string> del_space(vector<string> C) { 
+	int size1 = C.size();
+	for (int i = 0; i < size1; ++i) {
+		if (C[i][0] == ' ') {
+			C[i].erase(0, 1);
+		}
+	}
+	
+	return C;
+}
+
+vector<int> brack(vector<string> D, int num) { 
+	vector<int> brack_ind;
+	int size2 = D[num].size();
+	for (int k = 0; k < size2; ++k) {
+		if (D[num][k] == '(' || D[num][k] == ')') {
+			brack_ind.push_back(k);
+		}
+	}
+	return brack_ind;
+}
+
+void output_class(vector<string> F, int num, int s1, int s2) {
+	if (s1 > 0) {
+		int k = s1 - 1;
+		if (F[num][k] != ' ') {
+			for (int i = k; i <= s2; ++i) {
+				cout << F[num][i];
+			}
+			cout << endl;
+		}
+	}
+	else {
+		for (int i = s1; i <= s2; ++i) {
+			cout << F[num][i];
+		}
+		cout << endl;
+	}
+}
+
+
+vector<int> analysis(vector<string> def_class, vector<int> brack_ind, int num_of_row) {
+	bool var1 = false, var2 = false;
+	int ind_of_comm;
+	vector<int> index;
+	int size3 = brack_ind.size();
+	for (int i = 0; i < size3; i = i + 2) {
+		int pos1, pos2, pos8, pos9, pos10;
+		ind_of_comm = def_class[num_of_row].find(',', brack_ind[i]);
+		if (def_class[num_of_row][ind_of_comm - 1] == 'L' || def_class[num_of_row][ind_of_comm - 1] == 'l') {
+			pos1 = def_class[num_of_row].find('e', brack_ind[i]);
+			pos2 = def_class[num_of_row].find('.', brack_ind[i]);
+			if (pos1 == -1 && pos2 == -1) {
+				var1 = true;
+			}
+			else if (pos1 > ind_of_comm && pos2 > ind_of_comm){
+				var1 = true;
+			}
+			else if (pos1 > ind_of_comm && pos2 == -1) {
+				var1 = true;
+			}
+			else if (pos1 == -1 && pos2 > ind_of_comm) {
+				var1 = true;
+			}
+			else {
+				var1 = false;
+			}
+		}
+		else {
+			var1 = false;
+		}
+		
+		if (def_class[num_of_row][brack_ind[i + 1] - 1] == 'l' || def_class[num_of_row][brack_ind[i + 1] - 1] == 'L' || def_class[num_of_row][ind_of_comm + 2] == 'u' || def_class[num_of_row][ind_of_comm + 2] == 'U') {
+			pos8 = def_class[num_of_row].find('-', ind_of_comm);
+			pos9 = def_class[num_of_row].find('e', ind_of_comm);
+			pos10 = def_class[num_of_row].find('.', ind_of_comm);
+
+			if (pos8 == -1 && pos9 == -1 && pos10 == -1) {
+				var2 = true;
+			}
+			else if ((pos8 > ind_of_comm && pos8 < brack_ind[i + 1]) || (pos9 > ind_of_comm && pos9 < brack_ind[i + 1]) || (pos10 > ind_of_comm && pos10 < brack_ind[i + 1])) {
+				var2 = false;
+			}
+			else {
+				var2 = true;
+			}
+		}
+		else {
+			var2 = false;
+		}
+		if (var1 && var2) {
+			index.push_back(i);
+			index.push_back(i+1);
+		}
+	}
+	return index;
+}
+
+
+void function(vector<string> sym, vector<int> arr1, vector<int> arr2, int len) {
+	int size4 = arr1.size();
+	for (int i = 0; i < size4; ++i) {
+		if (arr2[i] == 0) {
+			sym[arr1[i]].erase(arr2[i], len);
+		}
+		else {
+			sym[arr1[i]].erase(0, arr2[i]);
+		}
+	}
+	vector<int> commas;
+	del_space(sym);
+	sym = del_space(sym);
+	//output(sym, sym.size());
+	ofstream out("result.txt");
+	vector<string> new_arr;
+	vector<int> numbers;
+	int size5 = arr1.size();
+	for (int i = 0; i < size5; ++i) {
+		vector<int> bc = brack(sym, arr1[i]);
+		numbers = analysis(sym, bc, arr1[i]);
+		//cout << numbers.size() << endl;
+		bool ind = false;
+		for (int k = 0; k < bc[numbers[0]]; ++k) {
+			if (sym[arr1[i]][k] == ',') {
+				ind = true;
+			}
+		}
+		//output(bc, bc.size());
+		//output(numbers, numbers.size());
+
+		vector<int> num_brack;
+		int n = numbers.size();
+		int n1 = bc.size();
+		for (int k = 0; k < n; ++k) {
+			for (int l = 0; l < n1; ++l) {
+				if (bc[numbers[k]] == bc[l]) {
+					num_brack.push_back(l);
+				}
+			}
+		}
+		//output(num_brack, num_brack.size());
+		if (!ind) {
+			if (numbers.size() / 2 == 1) {
+				for (int j = 0; j <= bc[numbers[1]]; ++j) {
+					out << sym[arr1[i]][j];
+				}
+				out << endl;
+			}
+			else {
+				int add = 1;
+				for (int l = 0; l <= bc[numbers[add]]; ++l) {
+					out << sym[arr1[i]][l];
+				}
+				out << endl;
+				for (int r = 3; r < n; r = r + 2) {
+					for (int e = bc[numbers[r] - 2] + 3; e <= bc[numbers[r]]; ++e) {
+						out << sym[arr1[i]][e];
+					}
+					out << endl;
+				}
+			}
+		}
+		else {
+			for (int r = 1; r < n; r = r + 2) {
+				for (int e = bc[numbers[r] - 2] + 3; e <= bc[numbers[r]]; ++e) {
+					out << sym[arr1[i]][e];
+				}
+				out << endl;
+			}
+		}
+	}
+}
+
+int main() {
+	ifstream in("input.txt");
+
+	string mes;
+	vector <string> arr;
+	while (!in.eof()) {
+		getline(in, mes);
+		arr.push_back(mes);
+	}
+	int arr_size = arr.size();
+	output(arr, arr_size);
+	
+	vector<string> del = delete_tab(arr, arr_size);
+	
+	int del_size = del.size();
+	output(del, del_size);
+
+	string lex = "complex";
+	vector<int> index;
+	vector<int> pos;
+	int val;
+	int sym_of_del = lex.length();
+	for (int i = 0; i < del_size; ++i) {
+		val = del[i].find(lex, 0);
+		if (val != -1) {
+			index.push_back(i);
+			pos.push_back(val);
+		}
+	}
+	
+	//output(index, index.size());
+	//output(pos, pos.size());
+	function(del, index, pos, sym_of_del);
+	
+	system("pause");
+	return 0;
+}
